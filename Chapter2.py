ab8a639d3caecf5e506e101923be356757770ebf
Prasanti Babu <54246449+prasantib@users.noreply.github.com>
2019-09-04 11:49:30 -0400

    Add files via upload

---
+++ Chapter2.py
@@ -0,0 +1,38 @@
+print("hello world!")
+for letter in "hello world!":
+    print(letter)
+message = "Hello Python world!"
+print(message)
+'I told my friend, "Python is my favorite language!"'
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print(full_name)
+
+print("Hello, " + full_name.title() + "!")
+greeting = "Hello, " + full_name.title() + "!"
+print(greeting)
+
+print("Python")
+#code used to create tabs
+print("\tPython")
+#code used to create a new line for each item
+print("Languages:\nPython\nC\nJavaScript")
+
+fav_lang = 'python '
+#getting rid of whitespaces in strings on right side of string
+fav_lang.rstrip()
+no_space = fav_lang.rstrip()
+print(no_space)
+
+all_space = '  python  '
+all_space.strip()
+print(all_space)
+print(1 + 5)
+
+import this
