8320691df1eb7285b92323f6ec32511d5fbe48cb
Prasanti Babu <54246449+prasantib@users.noreply.github.com>
2019-09-04 16:57:15 -0400

    Add files via upload

---
+++ employee_pay.py
@@ -0,0 +1,8 @@
+names = ['Mary','Jon','Bob','Mel','Jen', 'Sue', 'Ken', 'Dave', 'Beth', 'Ray']
+rates = [15, 22, 35, 43, 17, 29, 40, 20, 37, 16]
+hours = [40, 25, 4, 62, 33, 45, 36, 17, 37, 80]
+
+if hours in hours > 40:
+        print(hours * rates)
+else:
+    print(hours * rates)
