9a6b95bf6c18c9e2b122d3d8ab47c96c936d9a96
Prasanti Babu <54246449+prasantib@users.noreply.github.com>
2019-09-05 14:50:07 -0400

    Add files via upload

--- employee_pay.py
+++ employee_pay.py
@@ -1,8 +1,19 @@
-names = ['Mary','Jon','Bob','Mel','Jen', 'Sue', 'Ken', 'Dave', 'Beth', 'Ray']
-rates = [15, 22, 35, 43, 17, 29, 40, 20, 37, 16]
-hours = [40, 25, 4, 62, 33, 45, 36, 17, 37, 80]
+hours = int(input("Enter the number of hours worked: "))
+rate = int(input("What is the pay rate? "))
 
-if hours in hours > 40:
-        print(hours * rates)
+if hours > 40:
+        overtimeRate = 1.5 * rate
+        overtime = (hours - 40) * overtimeRate
+        hours = 40
 else:
-    print(hours * rates)
+        overtime = 0
+
+regular = hours * rate
+pay = regular + overtime
+
+print("You earned", (hours*3-min(40,hours))*rate/2)
+        
+
+
+
+
