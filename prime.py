6f2ade9ec5b4acaa29c14b87eff3b6b349cce8df
Prasanti Babu <54246449+prasantib@users.noreply.github.com>
2019-09-06 16:47:52 -0400

    Add files via upload

---
+++ prime.py
@@ -0,0 +1,11 @@
+def prime(value)
+value = int(input("Enter the last number in the range: "))
+
+for value in range()
+print(value)
+
+if n % 2 == 0:
+    print("\nThe number " + str(n) + " is not prime.")
+else:
+    print("\nThe number " + str(n) + " is prime.")
+
