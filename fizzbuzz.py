5e8855eb3aa485a924627640cf351c9b14ed943f
Prasanti Babu <54246449+prasantib@users.noreply.github.com>
2019-09-06 16:36:56 -0400

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,9 @@
+for value in range(101):
+    if value % 3 ==0 and value % 5 == 0:
+        print("fizzbuzz")
+    elif value % 3 == 0:
+        print("fizz")
+    elif value % 5 == 0:
+        print("buzz")
+    print(value)
+    
