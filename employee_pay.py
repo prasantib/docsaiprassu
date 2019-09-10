5a56d06a6c0a829acc3dca838c3a44f138f560a9
Prasanti Babu <54246449+prasantib@users.noreply.github.com>
2019-09-10 12:12:31 -0400

    Add files via upload

--- employee_pay.py
+++ employee_pay.py
@@ -1,18 +1,15 @@
 print("Welcome to the Employee Pay program!")
-id_num = ['001', '002', '003', '004', '005', '006', '007', '008', '009', '010']
-hours = ['Mary', 'John', 'Bob', 'Mel', 'Jen', 'Sue', 'Ken', 'Dave', 'Beth', 'Ray']
-rate = [15, 22, 35, 43, 17, 29, 40, 20, 37, 16]
-hours = [40, 25, 4, 62, 33, 45, 36, 17, 37, 80]
-overtime = []
 
-for hour in hours:
-        if hour > 40:
-                overtimeRate = 1.5 * int(rate)
-                overtime = (hour - 40) * overtimeRate
-                hours = 40
-                print(overtimeRate)
-        else:
-                print(rate)
+id_num = ['001', '002', '003', '004', '005', '006', '007', '008', '009', '010']
+name = ['Mary', 'John', 'Bob', 'Mel', 'Jen', 'Sue', 'Ken', 'Dave', 'Beth', 'Ray']
+rate = [15.0, 22.0, 35.0, 43.0, 17.0, 29.0, 40.0, 20.0, 37.0, 16.5]
+hour = [40, 25, 4, 62, 33, 45, 36, 17, 37, 80]
+employment_pay = [id_num, name, rate, hour]
+n = 0
+for n in range(10):
+        print(employment_pay[3][n])
+        overtime = 
+        
 
 
 
